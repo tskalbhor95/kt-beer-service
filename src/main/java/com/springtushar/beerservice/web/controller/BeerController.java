1f121425307e4db7aa424bec91ae14e0c167fc05
Tushar Kalbhor <tskalbhor95@gmail.com>
2024-02-12 16:16:59 +0530

    adding controller. closes #3

--- src/main/java/com/springtushar/beerservice/web/controller/BeerController.java
+++ src/main/java/com/springtushar/beerservice/web/controller/BeerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v2/beer")
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
  /*   private final BeerService beerService;
